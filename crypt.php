8baa68b6f02c3d31ab9c363a7964904cc8921ed8
darkj0n <sadolf12@gmail.com>
2019-09-02 00:42:18 +0300

    Update crypt.php
    -header added.
    -sha1 and base 64 added(with selectbox).
    -footer added.

--- crypt.php
+++ crypt.php
@@ -8,9 +8,36 @@
 		<title>MD5</title>
 		<meta charset="utf8">
 		<style>
+		
+			footer {
+			  position: fixed;
+			  left: 0;
+			  bottom: 0;
+			  width: 100%;
+			  text-align: center; 
+			}
+			
+			footer a {
+				text-decoration: none;
+				color: black;
+				font-family: "Courier New";
+			}
+			
+			footer a:hover {
+				color: skyblue;
+			}
+			
+			h1 {
+				margin: auto;
+				width: 250px;
+				font-family: Helvetica Neue,Helvetica,Arial,sans-serif;
+				color: skyblue;
+				margin-top: 30px;
+			}
+			
 			.main{
-				height: 150px;
-				width: 350px;
+				height: 200px;
+				width: 400px;
 				margin: auto;
 				position: relative;
 			}
@@ -32,29 +59,46 @@
 				cursor:pointer;
 			}
 			
+			.main select{
+				padding: 4px;
+				border: 1px solid #ddd
+			}
+			
 			.hash {
 				margin: auto;
 				position: relative;
-				height: 150px;
-				width: 380px;
+				height: auto;
+				width: auto;
 				font-family: "Courier New",Courier,monospace
 			}
 			
 			.hash span{
 				color: #5bc0de;
 			}
+
 		</style>
 	</head>
 
 	<body>
+		<h1>Hash Generator</h1><br><br>
 		<form method="get">
 			<div class="main">
 				<tr>
 					<td><input type="text" name="text" autocomplete="off"></td>
+					<td>
+						<select name="hashes">
+						  <option value="md5" selected>md5</option>
+						  <option value="sha1">sha1</option>
+						  <option value="base64">base64</option>
+						</select>
+					</td>
 					<td><input type="submit" value="generate"></td>
 				</tr>
 			</div>
 		</form>
+		<footer>
+			<p><a href="http://darkjon.tk" target="_blank">Xr00tgh0st</a></p>
+		</footer>
 	</body>
 
 </html>
@@ -62,12 +106,38 @@
 	
 	if($_GET){
 		
-		$pass = $_GET["text"];
-		if(empty($pass)){
-			echo "Boş yapma";
-		}else{
-			$hash = md5($pass);
-			echo "<div class='hash'>TEXT = "."<span style='width: 480px' onfocus='this.select();' onmouseup='return false;'>$hash</span></div>";
+		$hashes = $_GET['hashes'];
+		switch($hashes){
+			case 'md5':
+				$text = $_GET["text"];
+				if(empty($text)){
+					echo "Dont do this";
+				}else{
+					$hash = md5($text);
+					echo "<center><div class='hash'><label>$text = </label>"."<span style='width: 480px' onfocus='this.select();' onmouseup='return false;'>$hash</span></div></center>";
+				}
+			break;
+			
+			case 'sha1':
+				$text = $_GET["text"];
+				if(empty($text)){
+					echo "Boş yapma";
+				}else{
+					$hash = sha1($text);
+					echo "<center><div class='hash'><label>$text = </label>"."<span style='width: 480px' onfocus='this.select();' onmouseup='return false;'>$hash</span></div></center>";
+				}
+			break;
+			
+			case 'base64':
+				$text = $_GET["text"];
+				if(empty($text)){
+					echo "Boş yapma";
+				}else{
+					$hash = base64_encode($text);
+					echo "<center><div class='hash'><label>$text = </label>"."<span style='width: 480px' onfocus='this.select();' onmouseup='return false;'>$hash</span></div></center>";
+				}
+			break;
 		}
+		
 	}
 ?>
